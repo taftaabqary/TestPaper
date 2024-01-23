a33997c06206bea52e12c40f74e4e7e15bc52650
Muhammad Althaaf Abqary <93324343+taftaabqary@users.noreply.github.com>
2024-01-23 12:38:24 +0700

    Initial commit

---
+++ app/src/main/java/com/althaaf/testpaper/ContactActivity.kt
@@ -0,0 +1,11 @@
+package com.althaaf.testpaper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContactActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fo_activity_contact)
+    }
+}
